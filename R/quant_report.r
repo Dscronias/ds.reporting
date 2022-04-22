a8910f8fa76998885577bc24f431b6fdd789e1a2
Dimitri Scronias <scronias.dimitri@gmail.com>
2022-04-22 10:38:17 +0200

    New functions for quantitative vars

---
+++ R/quant_report.r
@@ -0,0 +1,56 @@
+#' Descriptive statistics for continuous variables
+#'
+#' This functions returns a dataframe with some descriptive statistics from continuous vars
+#' name of variables, labels, number of factor levels...
+#'
+#' @param data Dataframe to use
+#' @param columns String of list of strings with columns to describe
+#' @return A dataframe with descriptive statistics
+#' @export
+
+quant_report <- function(data, workbook, worksheet, vars, filename,
+    open_table_on_completion = FALSE) {
+
+    # HEADER STYLE
+    wb <- createWorkbook(workbook)
+    addWorksheet(wb, worksheet)
+
+    hs1 <- createStyle(
+        border = c("Top", "Bottom")
+    )
+    bs1 <- createStyle(
+        border = "Bottom"
+    )
+
+    # TABLE
+    report <- data %>% cont_summary(vars)
+
+    # TO EXCEL
+    ## TABLE
+    writeData(
+        wb = wb,
+        sheet = worksheet,
+        x = report
+    )
+    ## STYLE
+    ### HEADER
+    addStyle(
+        wb,
+        worksheet,
+        cols = 1:(length(report)),
+        rows = 1,
+        style = hs1
+    )
+    ### FOOTER
+    addStyle(
+        wb,
+        worksheet,
+        cols = 1:(length(report)),
+        rows = nrow(report)+1,
+        style = bs1
+    )
+
+    # WRITE TABLE
+    if (open_table_on_completion) {openXL(wb)}
+    saveWorkbook(wb, filename, overwrite = TRUE)
+}
---
+++ R/quant_summary.r
@@ -0,0 +1,41 @@
+#' Descriptive statistics for continuous variables
+#'
+#' This functions returns a dataframe with some descriptive statistics from continuous vars
+#' name of variables, labels, number of factor levels...
+#'
+#' @param data Dataframe to use
+#' @param columns String of list of strings with columns to describe
+#' @return A dataframe with descriptive statistics
+#' @export
+
+quant_summary <- function(data, columns) {
+    data %>%
+        summarise(
+            across(
+                all_of(columns),
+                list(
+                    N = ~ na.omit(.x) %>% length(),
+                    Mean = ~ mean(.x, na.rm = TRUE),
+                    SD = ~ sd(.x, na.rm = TRUE),
+                    Minimum = ~ min(.x, na.rm = TRUE),
+                    `1%` = ~ quantile(.x, 0.01, na.rm = TRUE),
+                    `5%` = ~ quantile(.x, 0.05, na.rm = TRUE),
+                    `25%` = ~ quantile(.x, 0.25, na.rm = TRUE),
+                    `50%` = ~ quantile(.x, 0.50, na.rm = TRUE),
+                    `75%` = ~ quantile(.x, 0.75, na.rm = TRUE),
+                    `95%` = ~ quantile(.x, 0.95, na.rm = TRUE),
+                    `99%` = ~ quantile(.x, 0.99, na.rm = TRUE),
+                    Maximum = ~ max(.x, na.rm = TRUE)
+                )
+            )
+        ) %>%
+        pivot_longer(
+            cols = everything(),
+            names_sep = "_",
+            names_to = c("Variable", ".value")
+        ) %>%
+        mutate(
+            Missing = glue('{round((nrow(df)-N)/nrow(df)*100, 2)}%'),
+            .after = N
+        )
+}
