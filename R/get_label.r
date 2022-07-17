57bc3aea0a8064447ce7a7c3a70997550f81c4b0
Dimitri Scronias <scronias.dimitri@gmail.com>
2022-07-17 14:39:19 +0200

    get_label function

---
+++ R/get_label.r
@@ -0,0 +1,24 @@
+#' Replace a variable name (var) with a label in another dataframe
+#'
+#' In oneway or twoway tables, replace the name of the variable with its label
+#' The label must be stored in another dataframe, with: a column with the list of variables
+#' and another column with its associated label
+#' @param data dataframe
+#' @param var Variable to rename
+#' @param data_label dataframe with the list of variables in data (in one column), and their label (in another column)
+#' @param label_from column in data_label of variables from data
+#' @param label_to column in data_label of labels
+#' @return Same table, with a renamed variable
+#' @export
+
+put_label <- function(data, var, data_label, label_from, label_to) {
+    var_label <- data_label %>% 
+        filter({{label_from}} == englue("{{var}}")) %>% 
+        pull({{label_to}})
+
+    data <- data %>%
+        rename(
+            !!sym(var_label) := {{var}}
+        )
+    return(data)
+}
