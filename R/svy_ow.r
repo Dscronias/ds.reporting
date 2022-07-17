da6b2d468ac59d88c7739f40b6f313a2c1e81e81
Dimitri Scronias <scronias.dimitri@gmail.com>
2022-07-17 12:40:20 +0200

    New params svy_ow

--- R/svy_ow.r
+++ R/svy_ow.r
@@ -6,17 +6,36 @@
 #' @param Data dataframe
 #' @param var Variable to use
 #' @param rounding number of digits for rounding (int)
+#' @param hide_prct_char hides the "%" characted in the percentages column
+#' @param lang changes the name of the percentage column (depending on your language). Three values : "en" (default), "fr", or "math"
 #' @return Count table
 #' @export
 
-svy_ow <- function(data, var, rounding = 2) {
+svy_ow <- function(data, var, rounding = 2, hide_prct_char = TRUE, lang="en") {
+    # Select header name
+    prct <- switch(
+        lang,
+        "en" = "Percentage",
+        "fr" = "Pourcentage",
+        "math" = "%"
+    )
+
     table <- data %>%
         survey_count({{var}}, .drop = FALSE) %>%
         select(-n_se) %>%
         mutate(
-            percent = n / sum(n)
+            {{prct}} := n / sum(n)
         ) %>%
-        adorn_pct_formatting(rounding, , TRUE, percent)
+        adorn_pct_formatting(rounding, , TRUE, {{prct}}) %>%
+        {
+            if (hide_prct_char)
+                mutate(
+                    .,
+                    {{prct}} := str_remove(.data[[prct]], "%")
+                )
+            else .
+        } %>%
+        rename(N = n)
 
     return(table)
 }
